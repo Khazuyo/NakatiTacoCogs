0e9a304ad2b042f5fd65c56dce007e0db67ca723
Khazuyo 'vali <khazuyovali@gmail.com>
2021-10-05 20:10:47 +0200

    + Adding core and basics of the Cutie cog

---
+++ cute/__init__.py
@@ -0,0 +1,4 @@
+from .main import Cute
+
+def setup(bot,):
+    bot.add_cog(Cute(bot))
---
+++ cute/main.py
@@ -0,0 +1,82 @@
+import dataclasses
+import discord
+import logging
+import random
+
+from redbot.core.commands import Cog
+from redbot.core import Config
+from redbot.core.data_manager import bundled_data_path
+
+class Cute(Cog, Settings, GroupCommands):
+
+	def __init__(self, bot, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		self.bot = bot
+
+		# Config setup (identifier isn't random btw :3)
+		self.config = Config.get_conf(self, identifier=211803212005, force_registration=True,)
+
+		default_global = {
+
+		}
+		self.config.register_global(**default_global)
+
+		default_guild = {
+			"message_history_depth": 100,
+			"cutie_lifetime_seconds": 60,
+			"cutie_last_picked_at": 0´,
+			"cutie_id": 0
+		}
+		self.config.register_guild(**default_guild)
+
+	@commands.command()
+	async def cute(self, ctx):
+    	current_cutie_id = await self.config.guild(ctx.guild).current_cutie_id()
+    	cutie_lifetime_seconds = await self.config.guild(ctx.guild).cutie_lifetime_seconds()
+    	cutie_last_picked_at = await self.config.guild(ctx.guild).cutie_last_picked_at()
+    	
+    	# Read current-cutie-id, compare with command timestamp
+    	# Read last n messages from channel and collect user IDs
+    	# Pick a random ID
+    	# Set current-cutie-id
+    	# Announce!
+
+    	# Do we need to pick a new cutie?
+    	cutieExpired = (cutie_last_picked_at + cutie_lifetime_seconds) > datetime.now().timestamp()
+    	weNeedToPickSomeone = (current_cutie_id == 0) or cutieExpired
+
+    	nameOfCutie = ""
+
+    	if weNeedToPickSomeone:
+    		message_history_depth = self.config.guild(ctx.guild).message_history_depth()
+
+    		allNLastAuthors = {}
+    		async for msg in ctx.channel.history(limit=message_history_depth):
+    			allNLastAuthors[msg.author.id] = msg.author
+
+    		if len(allNLastAuthors) < 1:
+    			nameOfCutie = "Nobody"
+
+    		else:
+	    		newCutie = random.choice(allNLastAuthors.values())
+
+	    		if newCutie == self.bot:
+	    			nameOfCutie = "I"
+	    		else:
+	    			nameOfCutie = newCutie.display_name
+
+    			self.config.guild(ctx.guild).current_cutie_id.set(newCutie.id)
+	    		self.config.guild(ctx.guild).cutie_last_picked_at.set(datetime.now().timestamp())
+
+    	else:
+    		# Just get the user's name
+    		try:
+    			cutieUser = ctx.guild.fetch_user(current_cutie_id)
+    			nameOfCutie = cutieUser.display_name
+    		except NotFound:
+    			nameOfCutie = "Anonymous"
+
+    	cutieAnnouncement = "{} is the Cutie of the Server right now!".format(nameOfCutie)
+
+    	await ctx.send(cutieAnnouncement)
